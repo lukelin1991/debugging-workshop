4d110a3f0707806f21b0de09e378104a3ee1ae1a
github username <github email address>
2020-02-25 11:41:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  // check this later, why is it empty?
+
   let joke;
 
+  console.log({
+    form: form
+  })
+
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
@@ -16,12 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // check this, what do we need an empty string for?
+    const username = document.getElementById('name-input').value
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
+
+    //we need the input where the name's being typed
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    fetchJoke().then(() => {
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
   })
 })
